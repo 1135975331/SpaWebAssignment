799ba576557c236e24b5950525b98d2f8ddc6276
1135975331 <dkswnstj021@gmail.com>
2023-05-31 01:44:48 +0900

    1

---
+++ MainJS.js
@@ -0,0 +1,13 @@
+$('document').ready(function() {
+    let buttons = $('.button')
+    
+    $('#aboutMe').click(function(event) {
+        console.log("abountMe Clicked")
+    })
+    $('#githubCommits').click(function(event) {
+        console.log("githubCommits Clicked")
+    })
+    $('#third').click(function(event) {
+        console.log("third Clicked")
+    })
+})
