8b2577ba2dbf5c95ecc88b4ca6c4a63344ef01f5
1135975331 <dkswnstj021@gmail.com>
2023-06-02 02:58:18 +0900

    GitHub 프로필에 이메일, 지역이 기재되어 있지 않은 경우 No Email/Location Provided 가 출력되도록 함

--- js/GithubActivityScript.js
+++ js/GithubActivityScript.js
@@ -5,11 +5,27 @@ function modifyHTMLGithub() {
     $('#name>a').html(userData.name)
     $('#name>a').attr('href', `https://github.com/${userData.name}`)
     $('#bio').html(`"${userData.bio}"`)
-    // $('#email').html("Email: " + userData.email)
-    $('#email').html("Email: 1135975331@kangwon.ac.kr")
-    $('#location').html("Location: " + userData.location)
+    $('#email').html("Email: " + getEmail(userData))
+    // $('#email').html("Email: 1135975331@kangwon.ac.kr")
+    $('#location').html("Location: " + getLocation(userData))
     $('#followers').html("Followers: " + userData.followers)
     $('#following').html("Following: " + userData.following)
     $('#created').html("Account Created: " + userData.account_created)
     $('#updated').html("Account Updated: " + userData.account_updated)
+}
+
+function getEmail(userData) {
+    if(userData.name === "1135975331")
+        return "1135975331@kangwon.ac.kr"
+    else if(userData.email === null)
+        return `<i>No Email Provided</i>`
+    else
+        return userData.email
+}
+
+function getLocation(userData) {
+    if(userData.location === null)
+        return `<i>No Location Provided</i>`
+    else
+        return userData.location
 }
