72c9a2c1d44a1f7ce2566696522bb6e9f2c0cb66
1135975331 <dkswnstj021@gmail.com>
2023-06-01 18:00:58 +0900

    프로필 정보 저장 관련 메소드를 해당 파일로 옮김

---
+++ js/InitUserData.js
@@ -0,0 +1,38 @@
+﻿async function initUserData() {
+    await $.ajax({ url: 'https://api.github.com/users/1135975331', success: function(data) {
+            // console.log(data)
+            setUserData(userData, data)
+        } });
+
+    await $.ajax({ url: `${userData.repos_data_url}`, success: function(data) {
+            // console.log(data);
+            userData.repos = [];
+            for(let i=0; i<data.length; i++) {
+                let repoData = data[i];
+                let userRepoDataInst = new UserRepoData(repoData);
+                userData.repos.push(userRepoDataInst)
+            }
+        } });
+}
+
+function setUserData(userData, data) {
+    userData.name = data.login;
+    userData.avatar_url = data.avatar_url;
+    userData.email = data.email;
+    userData.location = data.location;
+    userData.bio = data.bio;
+    userData.followers = data.followers;
+    userData.following = data.following;
+
+    userData.account_created = removeUnnecessaryChar(`${data.created_at}`);
+    userData.account_updated = removeUnnecessaryChar(`${data.updated_at}`);
+
+
+    userData.repos_data_url = data.repos_url;
+}
+
+function removeUnnecessaryChar(timeStr/*: string*/) {
+    if(typeof(timeStr) !== 'string') return "";
+    
+    return timeStr.replace('T',' ').replace('Z', '');
+}
